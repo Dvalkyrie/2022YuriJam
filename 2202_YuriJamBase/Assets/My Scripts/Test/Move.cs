291238829e99a0a31976ed433b636b1069ff6a04
SSU <chuang1990@users.noreply.github.com>
2022-10-15 14:41:38 -0400

    Add a simple test scene and minimal functionalities

---
+++ 2202_YuriJamBase/Assets/My Scripts/Test/Move.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    //Setup variables
+    private Animator Anim;
+    private AnimatorStateInfo Player1Layer0;
+    public float WalkSpeed = 0.0025f;
+    public float JumpSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player1Layer0.IsTag("Motion"))
+        {
+            // Move forward and backward
+            if(Input.GetAxis("Horizontal") > 0)
+            {
+                Anim.SetBool("Forward", true);
+                transform.Translate(-WalkSpeed, 0, 0);
+            }
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                Anim.SetBool("Backward", true);
+                transform.Translate(WalkSpeed, 0, 0);
+            }
+            if (Input.GetAxis("Horizontal") == 0)
+            {
+                Anim.SetBool("Forward", false);
+                Anim.SetBool("Backward", false);
+            }
+
+        }
+        
+        // Jump and Crouch
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            Anim.SetTrigger("Jump"); // not used yet
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            Anim.SetBool("Crouch", true);
+        }
+        if (Input.GetAxis("Vertical") == 0)
+        {
+            Anim.SetBool("Crouch", false);
+        }
+    }
+}
