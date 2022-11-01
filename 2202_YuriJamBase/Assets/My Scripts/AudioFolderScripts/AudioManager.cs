c60e6649959324db36093ba6bba5a22c4ec692a8
RapettiSpagetti <NoahBlueBox@gmail.com>
2022-11-01 18:59:15 +1100

    audiomanager script
    added a script that holds all audio and attaches them to a mixer

---
+++ 2202_YuriJamBase/Assets/My Scripts/AudioFolderScripts/AudioManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixerGroup audioMixer;
+
+    public Sound[] sounds;
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.outputAudioMixerGroup = audioMixer;
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void Start()
+    {
+        Play("AutoPlay1");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("sound: " + name + " not found!");
+        }
+        s.source.Play();
+    }
+}
+
