cc3a204fc1fe30d02e1b805b436c9f11cf285ad0
RapettiSpagetti <NoahBlueBox@gmail.com>
2022-11-17 21:59:00 +1100

    started music health
    made a music manager that holds music and starter programming music to change on health

---
+++ 2202_YuriJamBase/Assets/My Scripts/AudioFolderScripts/MusicMan.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Audio;
+
+public class MusicMan : MonoBehaviour
+{
+    public Sound[] sounds;
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            //s.source.outputAudioMixerGroup = AudioMixer();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void Update()
+    {
+        if (SaveScript.Player1Health > SaveScript.Player2Health)
+        {
+            Debug.Log("bam");
+        }
+    }
+    private void Start()
+    {
+        Play("DefaultTheme");
+        Play("BakerTheme");
+        Play("ChefTheme");
+
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("sound: " + name + " not found!");
+        }
+        s.source.Play();
+    }
+}
+
