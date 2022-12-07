7da92ac6e9bd9666a6924fa4a6959b39c96477fa
RapettiSpagetti <NoahBlueBox@gmail.com>
2022-12-08 08:27:23 +1100

    last minute bug fixs

--- 2202_YuriJamBase/Assets/SettingsMenu.cs
+++ 2202_YuriJamBase/Assets/SettingsMenu.cs
@@ -8,17 +8,31 @@ public class SettingsMenu : MonoBehaviour
     public AudioMixer MusicMixer;
     public AudioMixer SFXMixer;
     public AudioMixer VoiceMixer;
+
+    //private void Start()
+    //{
+    //    float Music = PlayerPrefs.GetFloat("MusicVol", 1f);
+    //    float Music2 = PlayerPrefs.GetFloat("SFXvol", 1f);
+    //    float Music3 = PlayerPrefs.GetFloat("volumeV", 1f);
+    //}
+
     public void SetVolumeM(float volumeM)
     {
         MusicMixer.SetFloat("MusicVol", volumeM);
+        PlayerPrefs.SetFloat("MusicVol", volumeM);
+        PlayerPrefs.Save();
     }
     public void SetVolumeSFX(float volumeSFX)
     {
         SFXMixer.SetFloat("SFXvol", volumeSFX);
+        PlayerPrefs.SetFloat("SFXVol", volumeSFX);
+        PlayerPrefs.Save();
     }
     public void SetVolumeV(float volumeV)
     {
         VoiceMixer.SetFloat("VoiVol", volumeV);
+        PlayerPrefs.SetFloat("VoiVol", volumeV);
+        PlayerPrefs.Save();
     }
     public void SetFullScreen (bool isFullscreen)
     {
